5690674657aa025bf062baafc9a8ce5022e0f8d8
ELAD1010 <elad2911@gmail.com>
2023-02-23 18:57:53 +0200

    add discord and jira bot config and add os utils

---
+++ src/common/types.ts
@@ -0,0 +1,1 @@
+export type AsyncVoidFunction = () => Promise<void>;
--- src/config/configurations.ts
+++ src/config/configurations.ts
@@ -1,10 +1,10 @@
-export interface AppConfigurations {
+export interface IAppConfigurations {
   discordBotToken: string | undefined;
   githubToken: string | undefined;
   jiraToken: string | undefined;
 }
 
-export default (): AppConfigurations => ({
+export default (): IAppConfigurations => ({
   discordBotToken: process.env.BOT_TOKEN,
   githubToken: process.env.GITHUB_TOKEN,
   jiraToken: process.env.JIRA_TOKEN,
